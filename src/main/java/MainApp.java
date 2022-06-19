7bad0cfd8500c5e314102a13cc0346d8dd4fdfb0
Racheal Gathagu <rachealkari@gmail.com>
2022-06-20 01:36:24 +0300

    adding printout for user input

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,6 +4,8 @@ public class MainApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter text to encrypt");
+        String inputText = scanner.nextLine();
 
     }
 }
