0f76bac3e8749a5732194389d25116597bdcd8fd
Racheal Gathagu <rachealkari@gmail.com>
2022-06-20 01:46:31 +0300

    update initial printed text

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,7 +4,9 @@ public class MainApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter text to encrypt");
+        System.out.println("Choose Option:");
+        System.out.println("1. Encrypt");
+        System.out.println("2. Decrypt");
         String inputText = scanner.nextLine();
 
     }
