f7e6a4b70be523fe59e984403b92d40dc4724506
Racheal Gathagu <rachealkari@gmail.com>
2022-06-20 01:53:48 +0300

    adding if statement for chosen options

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,10 +4,18 @@ public class MainApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose Option:");
+        System.out.println("Choose Option 1, 2 or 3:");
         System.out.println("1. Encrypt");
         System.out.println("2. Decrypt");
-        String inputText = scanner.nextLine();
+        System.out.println("3. Quit");
+        String option = scanner.nextLine();
 
+        if(option.equals(1)){
+
+        } else if (option.equals(2)) {
+
+        } else if (option.equals(3)) {
+            
+        }
     }
 }
