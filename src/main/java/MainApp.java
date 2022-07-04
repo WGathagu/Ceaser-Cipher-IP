dab4dc7899e0665deaf7f66383212dcb4afe156e
Racheal Gathagu <rachealkari@gmail.com>
2022-07-04 16:34:32 +0300

    fixing some syntax issues

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,18 +4,18 @@ public class MainApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome. Choose Option 1, 2 or 3:");
+        System.out.println("Welcome. Choose Option 1, 2 or 3: ");
         System.out.println("1. Encrypt");
         System.out.println("2. Decrypt");
         System.out.println("3. Quit");
         String option = scanner.nextLine();
 
         if(option.contains("1")){
-            System.out.println("Enter text to encrypt:");
+            System.out.println("Enter text to encrypt: ");
             String input = scanner.nextLine();
             System.out.println("___________________________");
 
-            System.out.println("Enter a key for the encryption (Key should be a number):");
+            System.out.println("Enter a key for the encryption (Key should be a number): ");
             int key = scanner.nextInt();
             System.out.println("___________________________");
             Encrypt encryptCipher = new Encrypt(input,key);
@@ -24,11 +24,11 @@ public class MainApp {
 
 
         } else if (option.contains("2")) {
-            System.out.println("Enter text to decrypt:");
+            System.out.println("Enter text to decrypt: ");
             String input = scanner.nextLine();
             System.out.println("___________________________");
 
-            System.out.println("Enter the key for the decryption:");
+            System.out.println("Enter the key for the decryption: ");
             int key = scanner.nextInt();
             System.out.println("___________________________");
             Decrypt decryptCipher = new Decrypt(input,key);
