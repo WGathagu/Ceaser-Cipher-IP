9353a43c6c7f2025d88716b92d3269ab67b2dc9a
Racheal Gathagu <rachealkari@gmail.com>
2022-07-04 16:17:55 +0300

    fixing bugs

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -4,26 +4,26 @@ public class MainApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose Option 1, 2 or 3:");
+        System.out.println("Welcome. Choose Option 1, 2 or 3:");
         System.out.println("1. Encrypt");
         System.out.println("2. Decrypt");
         System.out.println("3. Quit");
         String option = scanner.nextLine();
 
-        if(option.equals("1")){
+        if(option.contains("1")){
             System.out.println("Enter text to encrypt:");
             String input = scanner.nextLine();
             System.out.println("___________________________");
 
-            System.out.println("Enter a key for the encryption:");
+            System.out.println("Enter a key for the encryption (Key should be a number):");
             int key = scanner.nextInt();
             System.out.println("___________________________");
-            Encrypt cipher = new Encrypt(input,key);
-            String encrypted = Encrypt.encrypt(cipher);
+            Encrypt encryptCipher = new Encrypt(input,key);
+            String encrypted = Encrypt.encrypt(encryptCipher);
             System.out.println("Encrypted message: " + encrypted);
 
 
-        } else if (option.equals("2")) {
+        } else if (option.contains("2")) {
             System.out.println("Enter text to decrypt:");
             String input = scanner.nextLine();
             System.out.println("___________________________");
@@ -31,13 +31,13 @@ public class MainApp {
             System.out.println("Enter the key for the decryption:");
             int key = scanner.nextInt();
             System.out.println("___________________________");
-            Decrypt ceaser = new Decrypt(input,key);
-            String decrypted = Decrypt.decrypt(ceaser);
+            Decrypt decryptCipher = new Decrypt(input,key);
+            String decrypted = Decrypt.decrypt(decryptCipher);
 
             System.out.println("Decrypted message: " + decrypted);
 
 
-        } else if (option.equals("3")) {
+        } else if (option.contains("3")) {
             System.out.println("Exiting...");
         }
     }
